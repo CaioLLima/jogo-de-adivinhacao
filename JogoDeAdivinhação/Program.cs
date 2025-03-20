0cad5161a14f024eec02761b7cdf89f2a02172df
CaioLLima <caiololima2@gmail.com>
2025-03-20 20:28:12 -0300

    Etapa 3 - Verificar se o jogador acerto concluída.

--- JogoDeAdivinhação/Program.cs
+++ JogoDeAdivinhação/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace JogoDeAdivinhação
+﻿using System.Security.Cryptography;
+
+namespace JogoDeAdivinhação
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random geradorDeNumeros = new Random();
+            int numSecreto = geradorDeNumeros.Next(1, 21); ;
+
+            Console.WriteLine("Jogo da Adivinhação");
+
+            while (true)
+            {
+                
+                Console.Write("\nChute um número (1 à 20) para tentar adivinhar: ");
+                int numChute = Convert.ToInt32(Console.ReadLine());
+
+               
+
+                Console.WriteLine($"O número que você digitou é {numChute}.");
+                //Console.WriteLine($"O número secreto é {numSecreto}");
+
+                if(numChute == numSecreto)
+                {
+                    Console.WriteLine("\nParabéns!! Você acertou o número.");
+                    Console.ReadLine();
+                    break;
+                    
+                }
+                else if (numChute > numSecreto)
+                { 
+                    Console.WriteLine("\nVocê chutou um número maior, tente novamente.");
+                    continue;
+                }
+
+                else if (numChute < numSecreto)
+                {
+                    Console.WriteLine("\nVocê chutou um número menor, tente novamente.");
+                    continue;
+                }
+
+
+                Console.Write("Deseja continuar? (S/N)");
+                string continuar = Console.ReadLine().ToUpper();
+
+                if(continuar != "S")
+                {
+                    break;
+                }
+            }
         }
     }
 }
