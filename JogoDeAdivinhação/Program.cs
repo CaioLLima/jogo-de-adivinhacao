52e83d0b766ed749e49301527108697946a0ea89
CaioLLima <caiololima2@gmail.com>
2025-03-20 20:52:22 -0300

    Etapa 4 - Adicionado escolha de nível de dificuldade.

--- JogoDeAdivinhação/Program.cs
+++ JogoDeAdivinhação/Program.cs
@@ -7,49 +7,66 @@ namespace JogoDeAdivinhação
         static void Main(string[] args)
         {
             Random geradorDeNumeros = new Random();
-            int numSecreto = geradorDeNumeros.Next(1, 21); ;
+            int numSecreto = geradorDeNumeros.Next(1, 51);
+            int numTentativas = 5;
 
             Console.WriteLine("Jogo da Adivinhação");
 
-            while (true)
-            {
-                
-                Console.Write("\nChute um número (1 à 20) para tentar adivinhar: ");
-                int numChute = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEscolha um nível de dificuldade.\n 1 - Fácil(8 Tentativas) \n 2 - Normal(6 Tentativas) \n 3 - Difícil(4 Tentativas)");
+            int escolhaDificuldade = Convert.ToInt32(Console.ReadLine());
 
-               
+            if (escolhaDificuldade == 1)
+                numTentativas = 10;
 
-                Console.WriteLine($"O número que você digitou é {numChute}.");
-                //Console.WriteLine($"O número secreto é {numSecreto}");
+            else if (escolhaDificuldade == 2)
+                numTentativas = 7;
 
-                if(numChute == numSecreto)
+            else
+                numTentativas = 4;
+
+            while (numTentativas > 0)
                 {
-                    Console.WriteLine("\nParabéns!! Você acertou o número.");
-                    Console.ReadLine();
-                    break;
-                    
-                }
-                else if (numChute > numSecreto)
-                { 
-                    Console.WriteLine("\nVocê chutou um número maior, tente novamente.");
-                    continue;
-                }
 
-                else if (numChute < numSecreto)
-                {
-                    Console.WriteLine("\nVocê chutou um número menor, tente novamente.");
-                    continue;
+                    Console.Write("\nChute um número (1 à 50) para tentar adivinhar: ");
+                    int numChute = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine($"\nO número que você digitou é {numChute}.");
+                    //Console.WriteLine($"O número secreto é {numSecreto}");
+
+                    if (numChute == numSecreto)
+                    {
+                        Console.WriteLine("\nParabéns!! Você acertou o número.");
+                        Console.ReadLine();
+                        break;
+
+                    }
+                    else if (numChute > numSecreto)
+                    {
+                        numTentativas--;
+                        Console.WriteLine($"\nVocê chutou um número maior, tente novamente. Você possui {numTentativas} tentativas.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    }
+
+                    else if (numChute < numSecreto)
+                    {
+                        numTentativas--;
+                        Console.WriteLine($"\nVocê chutou um número menor, tente novamente. Você possui {numTentativas} tentativas.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    }
+
+
+                    /*Console.Write("Deseja continuar? (S/N)");
+                    string continuar = Console.ReadLine().ToUpper();
+
+                    if(continuar != "S")
+                    {
+                        break;
+                    }*/
                 }
-
-
-                Console.Write("Deseja continuar? (S/N)");
-                string continuar = Console.ReadLine().ToUpper();
-
-                if(continuar != "S")
-                {
-                    break;
-                }
-            }
         }
     }
 }
