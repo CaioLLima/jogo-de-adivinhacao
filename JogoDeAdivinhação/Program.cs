6be898947a3c4a3a6d8f7a1eb6494b718a47c5a8
CaioLLima <caiololima2@gmail.com>
2025-03-24 00:19:18 -0300

    Sistema de pontuação e de verificação de números repetidos.

--- JogoDeAdivinhação/Program.cs
+++ JogoDeAdivinhação/Program.cs
@@ -9,13 +9,16 @@ namespace JogoDeAdivinhação
             Random geradorDeNumeros = new Random();
             int numSecreto = geradorDeNumeros.Next(1, 51);
             int numTentativas;
-            int[] histChutes = new int[10];
+            int[] histChutes = new int[8];
             int contChutes = 0;
+            
 
             //pontuação 1000  -   (numero chutado - numero secreto) / 2      -   valor absoluto se valor for negatico
 
             while (true)
-                {
+            {
+                int pontuacao = 1000;
+                int vaPontucao;
                 Console.WriteLine("Jogo da Adivinhação");
 
                 Console.WriteLine("\nEscolha um nível de dificuldade.\n 1 - Fácil(8 Tentativas) \n 2 - Normal(6 Tentativas) \n 3 - Difícil(4 Tentativas)");
@@ -33,7 +36,15 @@ namespace JogoDeAdivinhação
                     Console.Write("\nChute um número (1 à 50) para tentar adivinhar: ");
                     int numChute = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine($"\nO número que você digitou é {numChute}.");
+                    for (int j = 0; j < histChutes.Length; j++)
+                    {
+                        if (histChutes[j] == numChute)
+                        {
+                            Console.WriteLine($"\nO número {numChute} já foi chutado. Tente novamente.");
+                            Console.Write("\nChute um número (1 à 50) para tentar adivinhar: ");
+                            numChute = Convert.ToInt32(Console.ReadLine());
+                        }
+                    }
 
                     if (numChute == numSecreto)
                     {
@@ -45,9 +56,11 @@ namespace JogoDeAdivinhação
                     else if (numChute > numSecreto)
                     {
                         numTentativas--;
-                        Console.WriteLine($"\nVocê chutou um número maior, tente novamente. Você possui {numTentativas} tentativas.");
+                        Console.WriteLine($"\nVocê chutou um número maior, tente novamente. Você possui {numTentativas} tentativas. Pressione ENTER para prosseguir...");
                         Console.ReadLine();
                         Console.Clear();
+                        vaPontucao = (numChute - numSecreto) / 2;
+                        pontuacao = pontuacao - Math.Abs(vaPontucao);
                         histChutes[contChutes] = numChute;
                         contChutes++;
                         continue;
@@ -56,24 +69,29 @@ namespace JogoDeAdivinhação
                     else if (numChute < numSecreto)
                     {
                         numTentativas--;
-                        Console.WriteLine($"\nVocê chutou um número menor, tente novamente. Você possui {numTentativas} tentativas.");
+                        Console.WriteLine($"\nVocê chutou um número menor, tente novamente. Você possui {numTentativas} tentativas. Pressione ENTER para prosseguir...");
                         Console.ReadLine();
                         Console.Clear();
+                        vaPontucao = (numChute - numSecreto) / 2;
+                        pontuacao = pontuacao - Math.Abs(vaPontucao);
                         histChutes[contChutes] = numChute;
                         contChutes++;
                         continue;
                     }
-                    
+
+
                 }
 
                 Console.WriteLine("\nSeu número de tentativas chegou a 0, você perdeu.");
-                
-                    Console.Write("\nDeseja jogar novamente? (S/N)");
-                    string continuar = Console.ReadLine().ToUpper();
+                Console.WriteLine($"\nSua pontuação foi: {pontuacao}");
 
-                    if(continuar != "S")
-                        break;
-                }
+                Console.Write("\nDeseja jogar novamente? (S/N)");
+                string continuar = Console.ReadLine().ToUpper();
+                Console.Clear();
+
+                if (continuar != "S")
+                    break;
+            }
         }
     }
 }
